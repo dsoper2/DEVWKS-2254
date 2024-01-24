f33a875f2c0a97c211e16ef30494d9148a505f79
“dsoper2” <dsoper@cisco.com>
2024-01-24 17:43:59 -0600

    CLive 2024 and .NET 8 updates

--- src/conf.sh
+++ src/conf.sh
@@ -6,7 +6,7 @@ caddy run &
 
 export PATH="$PATH:/home/developer/.dotnet:/home/developer/.dotnet/tools"
 export DOTNET_ROOT="/home/developer/.dotnet"
-dotnet tool install -g Microsoft.dotnet-interactive --version 1.0.317301
+dotnet tool install --global Microsoft.dotnet-interactive --version 1.0.506903
 dotnet-interactive jupyter install
 
 jupyter notebook &
