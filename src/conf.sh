515acc514486f2752064d14d89c5e631cf896f27
“dsoper2” <dsoper@cisco.com>
2024-01-27 16:13:37 -0600

    CLive2024 updates and fix for jupyter_server error

--- src/conf.sh
+++ src/conf.sh
@@ -9,4 +9,7 @@ export DOTNET_ROOT="/home/developer/.dotnet"
 dotnet tool install --global Microsoft.dotnet-interactive --version 1.0.506903
 dotnet-interactive jupyter install
 
+# fix for jupypter_server related errors
+sudo pip install traitlets==5.9.0
+
 jupyter notebook &
